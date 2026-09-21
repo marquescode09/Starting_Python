320b74b42baa48158577a93f1920245c594f7d1b
marquescode09 <guilhermemendonc28@gmail.com>
2026-09-21 20:04:07 -0300

    Update and rename converter_temp to converter_temp.py

--- converter_temp.py
+++ converter_temp.py
@@ -4,7 +4,7 @@ temperatura = float(input("Escreva a temperatura: "))
 if unidade == "C":
     Temperatura = round((9 * temperatura) / 5 + 32, 1)
     print(f'A temperatura em Fahreheit: {temperatura}°F')
-elif unit == "F":
+elif unidade == "F":
     temperatura = round((temperatura - 32) * 5 / 9, 1)
     print(f'A temperatura em Celsius: {temperatura}°C')
 else:
